dbb0e66e07bf2f33353c5d0bd04c51d35c3af70a
Pongor Ádám <93447735+ipowi01@users.noreply.github.com>
2025-10-15 16:21:07 +0200

    Add files via upload

---
+++ sor.c
@@ -0,0 +1,19 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(void) {
+    SetConsoleCP(1250), SetConsoleOutputCP(1250);  // initialization chained meaningfully
+
+    char* soer[] = {
+            "és nincs már egyetlen üveg sör sem a falon!",
+            "és már csak %d üveg sör áll a falon!"
+    };
+
+    for (int i = 100; i > 0;printf("%d üveg sör áll a falon,\n%d üveg sör!\n", i, i),
+                            printf("Leveszel egyet, körbeadod,\n"),
+                            printf(*(soer + (i > 1)), i-1),
+                            printf("\n\n"), i--);
+
+
+    return 0;
+}
